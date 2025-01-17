05a553a55aea898c15325083cd1637e8217a5a84
SadPanda <1459500+OG-Sadpanda@users.noreply.github.com>
2025-01-17 14:13:12 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -4,6 +4,9 @@
 cd ragnarok
 python3 -m venv venv
 source venv/bin/activate
+export LLAMA_CUBLAS=1
+CMAKE_ARGS="-DLLAMA_CUBLAS=on" pip3 install llama-cpp-python==0.2.26
+export CUDA_VISIBLE_DEVICES=0,1
 pip3 install -r requirements.txt
 
 # kick off the main app
