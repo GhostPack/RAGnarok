321d4853838303ac8285771a86d7ce6da5bb25c5
au70ma70n <168219140+au70ma70n@users.noreply.github.com>
2025-08-04 13:47:43 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -2,6 +2,7 @@
 
 # create the Python virtual env and install the requirements
 cd ragnarok
+sudo apt install python3.11-venv
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
